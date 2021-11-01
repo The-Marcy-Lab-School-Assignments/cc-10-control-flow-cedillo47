beee2ef15a14cdd8c8a2d9bd784cf6af4070aeb5
leo <cedillo47@gmail.com>
2021-11-01 13:53:07 +0000

    compleate

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 //1
-function greatestOfTwo(){
-  
+function greatestOfTwo(num1,num2){
+  if (num1 < num2){
+      return num2
+  } else {
+      return num1 
+  }
 }
-
+// console.log(greatestOfTwo(-10,2))
 //2
-function greatestOfThree(){
-  
+function greatestOfThree(num1,num2,num3){
+  if (num2 < num1 && num3 < num1 ){
+      return num1
+  } else if (num1 < num2 && num3 < num2){
+      return num2
+  } else {
+      return num3 
+  }
 }
-
+// console.log(greatestOfThree(10,20,100))
 //3
-function greatestOfFour(){
-  
+function greatestOfFour(num1,num2,num3,num4){
+  if (num2 < num1 && num3 < num1 && num4 < num1){
+      return num1 
+  } else if (num1 < num2 && num3 < num2 && num4 < num2){
+      return num2
+  } else if (num1 < num3 && num2 < num3 && num4 < num3){
+      return num3
+  } else {
+      return num4
+  }
 }
-
+// console.log(greatestOfFour(10,20,300,20000))
 
 //4
-function leastOfFour(){
-  
+function leastOfFour(num1,num2,num3,num4){
+  if (num1 < num2 && num1 < num3 && num1 < num4){
+      return num1 
+  } else if (num2 < num1 && num2 < num3 && num2 < num4){
+      return num2
+  } else if (num3 < num1 && num3 < num2 && num3 < num4){
+      return num3
+  } else {
+      return num4
+  }
 }
+
+// console.log(leastOfFour(12,112,300,40))
